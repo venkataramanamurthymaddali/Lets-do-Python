cba7c403c1d0438e51a7282a443b1c8e5e26c576
Venkata Ramana Murthy Maddali <vrmaddali@hotmail.com>
2025-08-09 10:29:49 +0530

    python data type

---
+++ data_typs.py
@@ -0,0 +1,9 @@
+name = "Ramana"
+age = 66
+print(name + "is" + str(age) + "years old")
+print(type(name))
+print("type of name is" + str(type(name)))
+name_type = type(name)
+name_type_str = str(name_type)
+print("type of name is " + name_type_str)
+print(f"{name} is {age} years old")
