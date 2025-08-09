c363df8afee62c938e600580a2fd0588adf6cf14
Venkata Ramana Murthy Maddali <vrmaddali@hotmail.com>
2025-08-09 22:12:43 +0530

    python arr anddict

---
+++ more_data_types.py
@@ -0,0 +1,31 @@
+# arr = []
+# arr.append("apple")
+# arr.append(234)
+# arr.append(123)
+# print(arr)
+# print(f"type of arr[1] is {type(arr[1])}")
+# print(f"type of arr[0] is {type(arr[0])}")
+# print(f"type of arr[2] is {type(arr[2])}")
+
+# marks = {}
+# print(type(marks))
+# marks["Mathematics"] = 94
+# marks["Science"] = 92
+# marks["SS"] = 80.5
+# marks[123] = 100
+
+# print(marks)
+# print(f"type of maths marks is {type(marks["Mathematics"])}")
+#print(f"type of ss marks is {type(marks["SS"])}")
+
+# List_to_sum = {}
+
+# arr = {1, 2, 3}
+# List_to_sum[arr] = 6
+
+# print(list_to_sum)
+
+int_to_arr = {}
+
+int_to_arr[1] = [1, 2, 3]
+print(int_to_arr)
